3f9a48174424466eda6bd5996180a20204b6416f
FT <fenauxft@gmail.com>
2026-05-09 19:21:48 +0200

    Use black signature favicon

--- www/actualités/index.php
+++ www/actualités/index.php
@@ -96,8 +96,10 @@ function shortDate($d) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Actualités | Xavier Fenaux · InteractivTrading</title>
-    <meta name="description" content="Rapports de journée, analyses et actualités des salles InteractivTrading.">
+<title>Actualités | Xavier Fenaux · InteractivTrading</title>
+    <link rel="icon" type="image/png" href="/images/favicon-signature-black.png">
+    <link rel="apple-touch-icon" href="/images/favicon-signature-black.png">
+<meta name="description" content="Rapports de journée, analyses et actualités des salles InteractivTrading.">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet">
